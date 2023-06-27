25d66206f96c262e1320c602c3aa1bc68f1c8f69
dmitrenko-31 <useroperator56@gmail.com>
2023-06-27 16:39:05 +0300

    [upd] Update sender module

--- python/data_sender.py
+++ python/data_sender.py
@@ -1,22 +1,29 @@
 from serial import Serial
+import config
 
 
 class Sender:
-
-    def __init__(self, bd_rate: int = 9600, usb_port: str = '/dev/ttyACM0', timeout: int = 1) -> None:
+    def __init__(
+        self,
+        bd_rate=config.SERIAL_BOUD_RATE,
+        usb_port=config.SERIAL_PORT,
+        timeout=config.SERIAL_TIMEOUT,
+    ) -> None:
         self.boud_rate = bd_rate
         self.usb_port = usb_port
         self.timeout = timeout
 
     def open_connect(self) -> None:
         try:
-            self.arduino = Serial(self.usb_port, self.boud_rate, self.timeout)
+            self.arduino = Serial(
+                port=self.usb_port, boudrate=self.boud_rate, timeout=self.timeout
+            )
         except:
             print("ERROR - Could not open USB serial port.")
 
     def send_command(self, command: str) -> bool:
         try:
-            self.arduino.write(command.encode(encoding='UTF-8'))
+            self.arduino.write(command.encode(encoding="UTF-8"))
             return True
         except:
             return False
@@ -25,4 +32,3 @@ class Sender:
 if __name__ == "__main__":
     sender = Sender()
     sender.open_connect()
-    
